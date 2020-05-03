4a3f29474746f02ac891479a807bee9ea5cca174
mystzd <grant@granthood.com>
2020-05-03 00:17:54 -0500

    fixed token

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  token: "NDUwNDE5MzA1NjcxMDMyODMy.Xq2ipw.49nkk11Uj4xRcse7PEKRuO-swTE",
-  yt_api_key: "AIzaSyCqli72lmo5jpNCJ5M8qfFJZCoMgZlq_TU",
-  ownerID: "426060491681431562",
-  prefix: "my."
-};
-
