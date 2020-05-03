5eae759cc511d52eb15d57d744e19dec771e79af
mystzd <grant@granthood.com>
2020-05-03 01:07:33 -0500

    bug fix attempt 4

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,22 +1,7 @@
-/*
- * pollen5/miyako
- * on github
- */
-const { RichEmbed } = require('discord.js');
 module.exports = async(message, guild) => {
 	if(!guild.available) return;
 	const channel = this.client.channels.get("706376972712017930");
-	if(!guild.owner && guild.ownerID) await guild.members.fetch(guild.ownerID);	
-
-	const em = new RichEmbed()
-	em.setTitle("mystbot joined a new server");
-	em.setDescription(guild.name);
-	em.setColor(0xFF00FF);
-	em.setThumbnail(guild.iconURL());
-	em.addField('Owner', guild.owner.user.tag);
-	em.addField('Member Count', guild.memberCount);
-	em.setFooter(`Mystbot v0.01`);
-	em.setTimestamp();
-
-	channel.send(em);
+	if(!guild.owner && guild.ownerID) await guild.members.fetch(guild.ownerID);
+	message.channel.send(`\`\`\`Mystbot has joined a server!\nServer Owner: ${guild.owner.user.tag}\nMember Count: ${guild.memberCount}\`\`\``);
 }
+
