c5e58fae28dcd0bf2db737c38c488f0183021e93
mystzd <grant@granthood.com>
2020-05-03 13:37:21 -0500

    fixed guildCreate

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,5 @@
-const Event = require('../base/Events.js');
-class GuildCreate extends Event {
-	async run(guild) {
-		if(!guild.available) return;
-		const channel = this.client.channels.get("706376972712017930");
-		await channel.send(`\`\`\`Mystbot has joined a server!\nServer Owner: ${guild.owner.user.tag}\nMember Count: ${guild.memberCount}\`\`\``);
-	}
+module.exports = async(message, guild, client) => {
+	if(!guild.available) return;
+	const channel = client.channels.get('706376972712017930');
+	channel.send(`\`\`\`Mystbot has joined a server.\nServer name: ${guild.name}\nServer Owner: ${guild.owner.user.tag}\nTotal Members: ${guild.memberCount}\`\`\``);
 }
-module.exports = GuildCreate;
-
